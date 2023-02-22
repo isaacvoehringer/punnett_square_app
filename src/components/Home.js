836497348bfdd8cce1567cf4b34222502bcf84be
Isaac Voehringer <isaacvoehringer@Isaacs-MacBook-Air.local>
2023-02-21 19:15:46 -0600

    set up

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+    return (
+      <div>
+        <h1></h1>
+        <div >
+          <img height="300" width="500" src="https://place-puppy.com/300x500" alt="puppy" />
+          <div>
+            
+          </div>
+        </div>
+        <a href="/Signup">
+          <button className="btn-primary">SignUpForm</button>
+        </a>
+      </div>
+    );
+  }
+  
+  export default Home;
