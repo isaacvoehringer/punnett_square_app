9b0f52f09a15f1f84ef82d7350463eda21280b27
sixtoherrera <sixto.herrera@continental.com>
2023-02-22 23:42:57 -0600

    added code to create database punnet square

---
+++ src/database/database-create.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE punnett_square
+with
+OWNER =  postgres
+ENCODING = 'UTF-8'		
+LC_COLLATE = 'en_US_UTF-8'			
+LC_CTYPE = 'en_US_UTF-8'
+TABLESPACE = pg_default
+CONNECTION LIMIT = -1
+comment on database db_name is 'punnett square database for final project'
